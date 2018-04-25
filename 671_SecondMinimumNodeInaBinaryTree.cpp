0ac883c190ade504b21fc3b7bc0d908bab5d79e3
PiscesDante <libingjian2580@163.com>
2018-04-25 21:49:42 +0800

    add files

---
+++ 671_SecondMinimumNodeInaBinaryTree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void findDifferentNumber(TreeNode* root) {
+        if (root == nullptr) return;
+        if (root->val > min_value) {
+            res = root->val;
+            return;
+        }
+        findDifferentNumber(root->left);
+        findDifferentNumber(root->right);
+    }
+    
+    void findSecondMinimum(TreeNode* root) {
+        if (root == nullptr) return;
+        if (root->val > min_value && root->val < res) res = root->val;
+        findSecondMinimum(root->left);
+        findSecondMinimum(root->right);
+    }
+    
+    int findSecondMinimumValue(TreeNode* root) {
+        min_value = root->val;
+        res = root->val;
+        
+        findDifferentNumber(root);
+        if (res == min_value) return -1;
+        findSecondMinimum(root);
+        return res;
+    }
+    
+private:
+    int min_value;
+    int res;
+};
+
+
+// 34 / 34 个通过测试用例
+// 状态：通过
+// 执行用时：12 ms
+// Your runtime beats 97.05 % of cpp submissions.
